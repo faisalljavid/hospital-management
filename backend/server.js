001f17d1798c9ca8be325ba0e36529dbcfad8ac9
Faisal Javid <faisalljavid@gmail.com>
2025-03-02 22:36:04 +0530

    Add render Api

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const cors = require('cors');
+app.use(cors({
+    origin: ['']
+}));
+
 // Routes  
 const hospitalRoutes = require('./routes/hospitalRoutes');
 app.use('/api/v1/hospitals', hospitalRoutes);
