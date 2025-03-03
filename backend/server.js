d40c78ac990d3e978fe5bbd578502e1b27319ebe
Faisal Javid <faisalljavid@gmail.com>
2025-03-03 22:28:59 +0530

    Accept requests from any origin

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://loquacious-manatee-f2c86e.netlify.app/'],
+    origin: '*',
     credentials: true
 }));
 
